34fb7277642db21da1d1380cd2fde45ce02542e0
unknown <770920755@qq.com>
2021-11-11 19:44:46 +0800

    change once

--- gitt/main.cpp
+++ gitt/main.cpp
@@ -1,9 +1,10 @@
-#include <iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
+    int a=1, b=2;
+    cout<<a+b<<'\n';
     cout << "Hello world!" << endl;
     return 0;
 }
